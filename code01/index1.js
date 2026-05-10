090920b7445411b8256f1555c215661a1af4bacc
sahilvish01 <sahilvish6542@gmail.com>
2026-05-11 00:06:47 +0530

    name property of function

---
+++ code01/index1.js
@@ -0,0 +1,13 @@
+// function ke paas .name property hoti hai jisme uss function ka name store ho jata hai
+
+const func =(() => {});
+
+console.log(func.name);
+
+
+function x()
+{
+    
+}
+
+console.log(x.name);
